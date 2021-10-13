75ca9a8b36dd2f3cb37a4f0580dc3707ea8c0ed6
sweera <sweerachahal95@gmail.com>
2021-10-12 21:12:54 -0700

    acquired credentials from api website

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?';
+let apiKey = '148f4dc0f989cfb31859a09b4509866d';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
