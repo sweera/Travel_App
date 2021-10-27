1545fb8d6b6a3b5f56e532146e1507255bb3e842
sweera <sweerachahal95@gmail.com>
2021-10-26 23:56:30 -0700

    postdata

--- server.js
+++ server.js
@@ -2,42 +2,41 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
 const port = 8000;
-const server = app.listen(port,listening);
-function listening(){
-    console.log("Server running");
-    console.log(`running on localhost: ${port}`);
+const server = app.listen(port, listening);
+function listening() {
+  console.log("Server running");
+  console.log(`running on localhost: ${port}`);
 }
 
 //GET request
 
 //let projectData = {};
 
-app.get('/all', (req, res) =>{
-    res.send(projectData);
-})
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
 
 //POST ROUTE
-app.post('/addInfo', (req, res) => {
-    projectData = req.body;
-    res.send({message: "Info received"})
-    console.log(req)
+app.post("/addInfo", (req, res) => {
+  projectData = req.body;
+  res.send({ message: "Info received" });
+  console.log(req);
 });
--- website/app.js
+++ website/app.js
@@ -1,55 +1,73 @@
 /* Global Variables */
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=148f4dc0f989cfb31859a09b4509866d';
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip";
+const apiKey = "&appid=148f4dc0f989cfb31859a09b4509866d";
 
-
+// Create a new date instance dynamically with JS
+const d = new Date();
+const newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 //Adding an event listener
-document.getElementById('generate').addEventListener('click', generateInfo);
+document.getElementById("generate").addEventListener("click", generateInfo);
 
 //Function called by event listener
-function generateInfo(){
-const zipcode = document.getElementById('zip').value;
-const feelings = document.getElementById('feelings').value;
-console.log(zipcode);
-console.log(feelings);
-if (zipcode !== ''){
+function generateInfo(e) {
+  e.preventDefault();
+  const zipcode = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+  console.log(zipcode);
+  console.log(feelings);
+  if (zipcode !== "") {
     const weatherurl = baseURL + zipcode + apiKey;
-    getWeatherData(weatherurl);
-}
+    getWeatherData(weatherurl).then(function (data) {
+      console.log(data);
+      postData("/addInfo", {
+        temperature: data.temperature,
+        date: newDate,
+        feelings: feelings,
+      });
+    });
+  }
 }
 
 //GET function to get API data
-const getWeatherData = async(weatherurl)=>{
-    const res = await fetch(weatherurl);
-    try{
-        const data = await res.json();
-        console.log(data);
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+const getWeatherData = async (weatherurl) => {
+  const res = await fetch(weatherurl);
+  try {
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 //Function to POST data
-const postData = async ( url = '', data = {})=>{
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    try{
-        const newData = await response.json();
-        console.log(newData)
-        return newData;
-    } catch(error) {
-        console.log("error", error);
-    }
-}
-// Create a new date instance dynamically with JS
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//Function to get project data
+const getprojectData = async (url) => {
+  const res = await fetch(url);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
+// Update UI
