ddc325d29248f0748fb5a8c369957853a2e82b2d
sweera <sweerachahal95@gmail.com>
2022-08-05 11:34:40 -0700

    dev and prod created

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,13 +3,19 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/client/index.js',
+    mode: "development",
+    devtool: "source-map",
     module: {
         rules: [
                 {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: "babel-loader"
-                }
+                },
+                {
+                    test: /\.scss$/,
+                    use: ["style-loader", "css-loader", "sass-loader"],
+                  },
         ]
 },
 plugins: [
