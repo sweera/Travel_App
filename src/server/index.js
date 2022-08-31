fc9c112e6859185e0f8f87dde863223968755baf
sweera <sweerachahal95@gmail.com>
2022-08-30 19:05:43 -0700

    working on api

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,15 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-//const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
-//const apiKey = "&appid=148f4dc0f989cfb31859a09b4509866d&units=imperial";
-//console.log(`Your api key is ${textapi}`);  // Used for testing API key entry
+const dotenv = require("dotenv");
+dotenv.config({path: ".env"});
+var path = require("path");
 
 // Require Express to run server and routes
 const express = require("express");
 
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,6 +21,16 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('dist'));
 
+
+//GET request
+
+// app.get("/all", (req, res) => {
+//   res.send(projectData);
+// });
+app.get('/', function(req,res){
+  res.sendFile('dist/index.html');
+})
+
 // Setup Server
 const port = 8000;
 const server = app.listen(port, listening);
@@ -28,15 +38,8 @@ function listening() {
   console.log("Server running");
   console.log(`running on localhost: ${port}`);
 }
-
-//GET request
-
-app.get("/all", (req, res) => {
-  res.send(projectData);
-});
-app.get('/', function(req,res){
-  res.sendFile('dist/index.html');
-})
+//********************GeoNames API********************/
+projectData = {};
 
 //POST ROUTE
 app.post("/addInfo", (req, res) => {
