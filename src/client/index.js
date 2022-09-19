c237bcfc0656ea01448346c9f9c75f4c24f2eac5
sweera <sweerachahal95@gmail.com>
2022-09-18 18:38:19 -0700

    updateui

--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,11 @@
 // alert("I exist");
 import { handleSubmit } from "./js/formHandler";
 import { findLength } from "./js/formHandler";
-
+import { checkValidity } from "./js/validityChecker";
 import './styles/style.scss';
 
 export {
     handleSubmit,
     findLength,
+    checkValidity,
 };
